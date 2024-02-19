9eca26c45afcb1497df43eb07af3821733753909
Dinil-Lamahewa <dinillamahewa00@gmail.com>
2024-02-20 01:45:31 +0530

    update my frontend Directory

---
+++ CSVToDjango/frontend/myreactapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUploader from './components/FileUploader';
+
+function App() {
+    return (
+        <div>
+            <h1 className='title'>CSV File Uploader</h1>
+            <FileUploader />
+        </div>
+    );
+}
+
+export default App;
